195302eb321ca66b83f43debecbab033c4c3ee22
Watson-Sei <seinabehack@gmail.com>
2022-07-23 15:00:40 +0900

    add babylon

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import * as BABYLON from 'babylonjs';
+
+    let canvas: HTMLCanvasElement;
+
+    const createScene = (canvas: HTMLCanvasElement) => {
+        const engine = new BABYLON.Engine(canvas, true);
+        const scene = new BABYLON.Scene(engine);
+        scene.clearColor = new BABYLON.Color4(0.9, 0.3, 0.3, 1);
+        
+        const camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, new BABYLON.Vector3(0, 0, -20), scene);
+        camera.setTarget(BABYLON.Vector3.Zero());
+        camera.attachControl(canvas, true);
+
+        const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, -15), scene);
+        light.intensity = 0.7;
+
+        const material = new BABYLON.StandardMaterial("material", scene);
+        material.emissiveColor = new BABYLON.Color3(0.3, 0.3, 0.3);
+
+        const cube = BABYLON.MeshBuilder.CreateBox("cube", {height: 5, width: 5, depth: 5}, scene);
+        cube.material = material;
+
+        engine.runRenderLoop(() => {
+            scene.render();
+        });
+
+        window.addEventListener('resize', () => {
+            engine.resize();
+        });
+
+        return scene;
+    }
+
+    onMount(() => {
+        createScene(canvas)
+    })
+</script>
+
+<canvas
+    bind:this={canvas}
+></canvas>
+
+<style>
+    canvas {
+        width: 100vw;
+        height: 100%;
+    }
+</style>
